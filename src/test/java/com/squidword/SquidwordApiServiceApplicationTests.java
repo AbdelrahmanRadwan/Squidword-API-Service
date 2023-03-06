29db77c711d8fadafd9967864715a3eb3ae61f56
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2023-03-06 17:52:35 +0000

    Added spring structure

---
+++ src/test/java/com/squidword/SquidwordApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squidword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidwordApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
